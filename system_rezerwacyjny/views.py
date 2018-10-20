61bb21a814e08775daaa245af958b9692f2151e5
Paulina Serotiuk <paulinaserotiuk@Paulinas-MacBook-Air.local>
2018-10-20 14:12:55 +0200

    views

--- system_rezerwacyjny/views.py
+++ system_rezerwacyjny/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from system_rezerwacyjny  import *
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, get_object_or_404
+
+def new_room(request):
+    pass
+
+def modify_room(request):
+    pass
+
+def delete_room(request):
+    pass
+
+def single_room(request):
+    pass
+
+def all_rooms(request):
+    pass
+
+
+
+
+
+
 
 # Create your views here.
