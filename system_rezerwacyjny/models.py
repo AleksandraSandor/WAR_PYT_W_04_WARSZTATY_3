788e8a71da86979cc60a0e8fdbee90e67d81f6c7
Aleksandra Sandor <aleksandra.walas87@gmail.com>
2018-10-20 13:04:35 +0200

    models created

--- system_rezerwacyjny/models.py
+++ system_rezerwacyjny/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Sala(models.Model):
+    name = models.CharField(max_length=128)
+    capacity = models.IntegerField()
+    is_projector = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "{} {} {}".format(self.name, self.capacity, self.is_projector)
+
+
+class Reservation(models.Model):
+    date = models.DateField(null=True)
+    id_sali = models.ForeignKey(Sala, on_delete=models.CASCADE)
+    note = models.TextField()
+
+    def __str__(self):
+        return "{} {} {}".format(self.date, self.id_sali, self.note)
+
+
