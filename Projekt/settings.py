b71b86a504045e44b3c0c52bbec3318e6027191e
Aleksandra Sandor <aleksandra.walas87@gmail.com>
2018-10-20 14:01:24 +0200

    new project

--- Projekt/settings.py
+++ Projekt/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'Projekt.wsgi.application'
 DATABASES = {
     'default': {
         'HOST': '127.0.0.1',
-        'NAME': 'postgres',
+        'NAME': 'sale_db',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'USER': 'aleksandrasandor',
         'PASSWORD': '1987lukas',
