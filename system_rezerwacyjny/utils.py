45aa0380f4c74b4464df2325f467f8f812cfbd7e
Sythrill <ksciwiarska@gmail.com>
2018-11-01 18:23:20 +0100

    marge reservation with search

--- system_rezerwacyjny/utils.py
+++ system_rezerwacyjny/utils.py
@@ -13,11 +13,14 @@ class MyCalendar(HTMLCalendar):
 
     def formatday(self, day, events):
         cssclass = self.cssclasses[events]
-        print(datetime.now())
         events = Reservation.objects.filter(date__year=self.room_id, date__month=self.month, id_sali=self.year)
         notes = events.filter(date__day=day, id_sali=self.year)
         d = ''
         form = ''
+        calendar_today = f'{self.room_id}-{self.month}-{day}'
+
+        day_today = f'{datetime.today().year}-{datetime.today().month}-{datetime.today().day}'
+
         for note in notes:
             if note.note !="":
                 d += f'<li> {note.note} </li>'
@@ -26,13 +29,15 @@ class MyCalendar(HTMLCalendar):
         if day == 0:
             return '<td class="noday">&nbsp;</td>'
         else:
-            if f'{datetime.today().year}-{datetime.today().month}-{datetime.today().day}' == f'{self.room_id}-{self.month}-{day}':
+            if day_today == calendar_today:
                 cssclass += ' today'
             if d != "":
                 cssclass += ' reserved'
-            if d == '':
+            if d == '' and datetime.strptime(calendar_today, '%Y-%m-%d') >= datetime.strptime(day_today, '%Y-%m-%d'):
                 form += f'''<form method="POST">
                 <input type="text" name="notes_{day}">
                 <button value="{self.room_id}-{self.month}-{day}" name="date" >Zarezerwuj</button>
                 <form>'''
+            if d == '' and datetime.strptime(calendar_today, '%Y-%m-%d') < datetime.strptime(day_today, '%Y-%m-%d'):
+                cssclass += ' old'
             return f'<td class="{cssclass}">{day}<ul>{d}</ul>{form}</td>'
